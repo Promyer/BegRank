c80a53beddfa3ecc3330dfac0026db568d901d44
promyer <ppinhalls@gmail.com>
2019-11-26 19:06:48 +0300

    Fix sh script

--- run_count_stats.sh
+++ run_count_stats.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-OUTDIR=out/sem3/wc
+OUTDIR=/user/miroslav.morozov/stats/
 hadoop fs -rm -r -f $OUTDIR
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 	-file mapper.py -file reducer.py \
-	-input '/data/seminar3/texts/*.txt' -output $OUTDIR \
+	-input '/tmp/miroslav.morozov/out/' -output $OUTDIR \
 	-mapper mapper.py -reducer reducer.py -combiner reducer.py
 
 echo job finished, see output in $OUTDIR
